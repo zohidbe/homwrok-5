420f96d96334a41ddd9ce48fc6eb5fe847326787
zohidbe <zohaxaydarov@gmail.com>
2024-08-07 09:56:14 +0500

    homwork end

---
+++ homwork.js
@@ -0,0 +1,40 @@
+// task 1/
+// let yozuv = "46541ssfsasef"
+// let yigindi = 0;
+
+// for (let i = 0; i < yozuv.length; i++) {
+//     let belgi = yozuv[i];
+//     if (!isNaN(belgi) && belgi != ''){
+//         yigindi += parseInt(belgi);
+//     }
+//     }
+
+//     console.log("yigindi: " + yigindi);
+    
+// task 2
+
+// bolen 17 misol
+
+// let son = 100;
+
+// let uchXonali = (son >= 100 && son <= 999);
+// console.log("berilgan son uch xonali:" + uchXonali);
+
+// task 3
+
+// let son = 123;
+
+// let yozuvlar = Math.floor(son / 100);
+// let onlar =  Math.floor((son % 100) / 10);
+// let birliklar = son % 10;
+
+// let barchaRaqam = (yozuvlar !== onlar) && (yozuvlar !== birliklar) && (onlar !== birliklar);
+// console.log("ushbu sonning barcha raqamlari har xil" + barchaRaqam);
+
+// task 4
+
+// let a = 5 , b = 8 , c = 8;
+
+// let hechBolmagandaIkkiTasiTeng = (a ===b) || (a === c) || (b === c) ;
+// console.log("Berilgan uchta butun sonlarning hech bo'lmaganda 2 tasi bir-biriga teng: " + hechBolmagandaIkkiTasiTeng);
+ 
---
+++ main.js
@@ -0,0 +1,124 @@
+// let a = 85;
+
+// for (let i = 1; i <= 10; i++) {
+//     if (a % i == 0) {
+//         console.log(i);
+//     }
+// };
+
+// task tub son topib berad
+
+// let num = 4512301514856213;
+// let coount = 0
+
+// for (let i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//         console.log(i);
+        
+//     }
+// }
+
+// let str = "molas"
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+    
+// }
+
+
+
+// multiple case switch program
+// let fruit = prompt("sorang");
+// switch(fruit) {
+//     case 'apple':
+//     case 'mango':
+//     case 'pineapple':
+//         console.log(`${fruit} is a fruit.`);
+//         break;
+//     default:
+//         console.log(`${fruit} is not a fruit.`);
+//         break;
+// }
+
+
+///
+// let fruit = prompt("sorang")
+
+// switch (fruit) {
+//     case'olma':
+//     case 'ananas':
+//     case'mango':
+//     console.log(`${fruit} is a fruit`);
+//     break;
+//     default:
+//     console.log(`${fruit} is not a fruit`);
+//     break;
+// }
+
+
+// // program for a simple calculator
+// let result;
+
+// // take the operator input
+// const operator = prompt('Enter operator ( tanglang +, -, * or / ): ');
+
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//         result = number1 + number2;
+//         console.log(`${number1} + ${number2} = ${result}`);
+//         break;
+//     case '-':
+//         result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         break;
+//     case '*':
+//         result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         break;
+//     case '/':
+//         result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+
+
+
+// // program to display numbers
+// let i = 1;
+// const n = 100000;
+
+// // do...while loop from 1 to 5
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= n);
+
+// let num = 1;
+// const n = 10;
+
+// do {
+//     console.log(num);
+//     num++;
+// } while(num <= n)
+
+
+    // let num = 4;
+    // let coount = 0
+
+    // for (let i = 1; i <= num; i++) {
+    //     if (num % i == 0) {
+    //         console.log(i);
+            
+    //     }
+    // }
+
+
+    
